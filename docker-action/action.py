8cb73a557f312419ea24974d3379768d03aadf40
Alexander Lerach <alexander@auterion.com>
2024-11-21 14:30:30 +0100

    Updated bloaty version, output total values

--- docker-action/action.py
+++ docker-action/action.py
@@ -111,6 +111,23 @@ def main():
             )
         )
 
+    # Get the TOTAL output
+    encoded_lines = encoded_output.splitlines()
+    if len(encoded_lines) < 1 or "TOTAL" not in encoded_lines[-1]:
+        print("\nAction:WARN: Could not find the total in bloaty output.", flush=True)
+        return 1
+
+    total_split = encoded_lines[-1].split()
+    if len(total_split) < 4:
+        print("\nAction:WARN: Could not split the total found in bloaty output into its individual parts.", flush=True)
+        return 1
+    
+    add_to_gh_env_var("GITHUB_OUTPUT", key="bloaty-total-file-percentage", value=total_split[0])
+    add_to_gh_env_var("GITHUB_OUTPUT", key="bloaty-total-file-absolute", value=total_split[1])
+    add_to_gh_env_var("GITHUB_OUTPUT", key="bloaty-total-vm-percentage", value=total_split[2])
+    add_to_gh_env_var("GITHUB_OUTPUT", key="bloaty-total-vm-absolute", value=total_split[3])
+
+    # Exit with success
     return 0
 
 
